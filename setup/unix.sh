6d213e43d91bb1243e825c695cd873f474590b03
Rafi <mohamedrafikhan2009@gmail.com>
2026-02-15 11:21:03 +0000

    Update unix.sh

--- setup/unix.sh
+++ setup/unix.sh
@@ -10,10 +10,10 @@ echo This might take a few moments depending on your internet speed.
 haxelib git linc_luajit https://github.com/MRK2009/linc_luajit --quiet #Sirox's linc_luajit (backup)
 haxelib git flixel https://github.com/PsychExtendedThings/flixel 5.6.1 --quiet
 haxelib install flixel-addons 3.2.2 --quiet
-haxelib install flixel-ui 2.4.0 --quiet #I don't know why is it exist
+haxelib install flixel-ui 2.6.0 --quiet #I don't know why is it exist
 haxelib git SScript https://github.com/PsychExtendedThings/SScript --quiet #stable SScript version (KarimAkra recommended this version)
 haxelib git hscript-improved https://github.com/PsychExtendedThings/hscript-improved-Psych --quiet #changed with updated one
-haxelib install hscript 2.4.0 --quiet
+haxelib install hscript 2.6.0 --quiet
 haxelib install tjson 1.4.0 --quiet #for fucking mods system (haxe.Json brokes the modlist)
 haxelib git hxCodec https://github.com/PsychExtendedThings/hxCodec-0.6.3 --quiet #That clearly my repo
 haxelib git hxcpp https://github.com/beihu235/hxcpp --quiet #Just a normal hxcpp
